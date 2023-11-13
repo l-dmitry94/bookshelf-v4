1d27353c209458f14d774d588d20f1061c6ae5f1
Dmytro Liuba <lyubadmitry2@gmail.com>
2023-11-13 15:24:20 +0200

    update files

--- src/js/mobile-menu/index.js
+++ src/js/mobile-menu/index.js
@@ -1,13 +1,23 @@
 import refs from "../refs";
 
+refs.closeIcon.style.display = "none"
+
+
+
 refs.burgerBtn.addEventListener("click", handleClick);
 
 function handleClick(event) {
     const isOpen = event.currentTarget.classList.toggle("active")
     
     if(isOpen) {
-        refs.mobileMenu.classList.add("active")
+        refs.mobileMenu.classList.add("active");
+        refs.closeIcon.style.display = "block";
+        refs.burgerIcon.style.display = "none"
+
+
     } else {
         refs.mobileMenu.classList.remove("active");
+        refs.closeIcon.style.display = "none";
+        refs.burgerIcon.style.display = "block"
     }
 }
--- src/js/refs/index.js
+++ src/js/refs/index.js
@@ -1,6 +1,8 @@
 const refs = {
     burgerBtn: document.querySelector('.header__burger'),
     mobileMenu: document.querySelector('.mobile-menu'),
+    burgerIcon: document.querySelector(".header__burger-icon-burger"),
+    closeIcon: document.querySelector(".header__burger-icon-close"),
 };
 
 export default refs;
