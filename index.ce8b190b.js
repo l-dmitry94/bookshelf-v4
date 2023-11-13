a8d84be8e62ce422d610b8039cfd1e9178d39a38
l-dmitry94 <64677316+l-dmitry94@users.noreply.github.com>
2023-11-13 12:50:10 +0000

    Deploying to gh-pages from @ l-dmitry94/bookshelf-v4@088cffbb8c47d97e753b6998286b1fb5a42f53f3 🚀

---
+++ index.ce8b190b.js
@@ -0,0 +1,2 @@
+!function(){var e={burgerBtn:document.querySelector(".header__burger"),mobileMenu:document.querySelector(".mobile-menu")};e.burgerBtn.addEventListener("click",(function(t){t.currentTarget.classList.toggle("active")?e.mobileMenu.classList.add("active"):e.mobileMenu.classList.remove("active")}))}();
+//# sourceMappingURL=index.ce8b190b.js.map
